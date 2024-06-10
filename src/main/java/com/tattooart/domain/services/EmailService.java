8d8bb440b1ea24296d1e7cfa89783e576f287363
Marina Rosillo <131238214+marinxrosillo@users.noreply.github.com>
2024-06-10 12:42:39 +0200

    Comment EmailService.java

--- src/main/java/com/tattooart/domain/services/EmailService.java
+++ src/main/java/com/tattooart/domain/services/EmailService.java
@@ -12,11 +12,15 @@ public class EmailService {
     private JavaMailSender javaMailSender;
 
     public void sendEmail(String to, String subject, String body) {
+	// Crear una nueva instancia de SimpleMailMessage
         SimpleMailMessage message = new SimpleMailMessage();
+	// Establecer el destinatario del correo electrónico
         message.setTo(to);
+	// Establecer el asunto del correo electrónico
         message.setSubject(subject);
+	// Establecer el cuerpo del correo electrónico
         message.setText(body);
+	// Enviar el correo electrónico utilizando JavaMailSender
         javaMailSender.send(message);
     }
-    
 }
